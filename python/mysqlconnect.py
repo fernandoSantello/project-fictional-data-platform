bf60ffe6a298a5a8462d622ef39b4a63a23d6f11
Fernando Santello Rodrigues Pereira <fersrp1964@gmail.com>
2024-02-12 12:10:15 -0300

    First draft of the mysql connector class

---
+++ python/mysqlconnect.py
@@ -0,0 +1,55 @@
+import mysql.connector
+from datetime import datetime as dt
+
+class MysqlDatabase:
+    def __init__(self, dbname: str, user: str, password: str, host: str):
+        self.conn = mysql.connector.connect(user=user, 
+                                      password=password,
+                                      host=host,
+                                      database=dbname)
+        self.cursor = self.conn.cursor()
+
+
+    def __enter__(self):
+        now = dt.now().strftime('%Y-%m-%d %H:%M:%S')      
+        return self, now
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+        
+    def select_statment(self, table_name: str, condition: bool, clause: str = None):
+            if not condition:
+                self.cursor.execute(f"SELECT * FROM {table_name}")
+                rows = self.cursor.fetchall()
+            else:
+                self.cursor.execute(f"SELECT * FROM {table_name} WHERE {clause}")
+                rows = self.cursor.fetchall()               
+            return rows
+    
+    def insert_statment(self, table_name: str, column_values: list):
+        sql = f'INSERT INTO {table_name} VALUES {column_values}'
+        self.cursor.execute(sql)
+
+
+    def delete_statment(self, table_name: str, condition: bool, clause: str = None):
+        if not condition:
+            sql_query = f"DELETE FROM {table_name}"
+            self.cursor.execute(sql_query) 
+        else:           
+            sql_query = f"DELETE FROM {table_name} WHERE {clause}"
+            self.cursor.execute(sql_query)            
+
+
+def main():
+    with MysqlDatabase(dbname='teste', user='user', password='user', host='127.0.0.1') as (database, now):
+        empresas = database.insert_row('teste_table', 3)
+
+        for emp in empresas:
+            print(emp)
+            print(now)
+
+
+if __name__ == '__main__':
+    main()
