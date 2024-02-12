0320c5147c700783fb95c03807c83f306161239c
Fernando Santello Rodrigues Pereira <fersrp1964@gmail.com>
2024-02-12 13:50:25 -0300

    First API draft

---
+++ python/api.py
@@ -0,0 +1,19 @@
+import requests
+
+def fetch_currency_data(currencies: list):
+    rows = []
+    for currency in currencies:
+        response = requests.get(f'https://api.coincap.io/v2/rates/{currency}')
+        if response.status_code ==200:
+            data = response.json()
+            data = data['data']
+
+            rows.append(data)
+        else:
+            rows.append([currency, None, None, None, None])
+    return rows
+            
+
+currency_data = fetch_currency_data(currencies=['bitcoin', 'ethereum'])
+
+print(currency_data)
