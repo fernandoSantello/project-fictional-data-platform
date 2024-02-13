a71371ac2d9a558dfadb97dc99b1d92192eb7473
Fernando Santello Rodrigues Pereira <fersrp1964@gmail.com>
2024-02-13 11:46:43 -0300

    Changed folder structure

--- python/main.py
+++ python/main.py
@@ -1,9 +1,9 @@
-from mysqlconnect import MysqlDatabase
-from api import API
+from database.mysqlconnect import MysqlDatabase
+from api.api import API
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def process():
+def main():
     database = MysqlDatabase()
     api = API()
     currency_data = api.fetch_currency_data(currencies=['bitcoin', 'ethereum'])
@@ -16,14 +16,6 @@ def process():
             else:
                 values = [row.get('id'), row.get('symbol'), row.get('currencySymbol'), row.get('type')]
                 database.insert_statment(table_name='currency', column_values=values)
-
-
-
-def main():
-    
-    process()
-
-
 
 
 if __name__ == '__main__':
