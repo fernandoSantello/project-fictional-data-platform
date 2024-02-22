36a1bc6da05cfe53e7f6d62ffa88ee1ed16e5aed
Fernando Santello Rodrigues Pereira <fersrp1964@gmail.com>
2024-02-22 15:02:34 -0300

    REFACTOR: Removed unnecessary blank lines

--- python/interfaces/database.py
+++ python/interfaces/database.py
@@ -1,11 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Union
 
-
-
 class Database(ABC):
     
-
     @abstractmethod
     def insert_statement(self, sql: tuple) -> None:
         pass
